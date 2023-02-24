f159d19a04ac487e58d61793cf8651531639ce5e
码魂 <pkptzx@gmail.com>
2023-02-25 07:36:34 +0800

    feat: add style by cdn

--- src/views/QRDecoder.vue
+++ src/views/QRDecoder.vue
@@ -1,14 +1,26 @@
 <template>
-  <div class="box">
+  <!-- <div class="box">
     <div ref="preview" v-on:paste="handlePaste" style="height: 300px;background-color: darkgray;text-align: center;">
       <span>将图片按Ctrl+V 粘贴至此处</span>
     </div>
-    <button @click="uploadPlans">上传并解析</button>
+    <button class="btn" @click="uploadPlans">上传并解析</button>
+  </div> -->
+
+  <div class="card card-compact w-96 bg-base-100 shadow-xl">
+  <figure ref="preview" v-on:paste="handlePaste"><div class="grid w-96 h-96 bg-base-300 place-items-center">将图片按Ctrl+V 粘贴至此处</div></figure>
+  <div class="card-body">
+    <h2 class="card-title">解析二维码</h2>
+    <p>如果二维码中是一个URL那么将会返回请求URL后的结果</p>
+    <div class="card-actions justify-end">
+      <button class="btn btn-primary"  @click="uploadPlans">上传并解析</button>
+    </div>
   </div>
+</div>
 </template>
 <script setup lang="ts">
 import { onMounted, onUnmounted, ref } from 'vue';
-
+//@ts-ignore
+import '//cdn.staticfile.org/sweetalert2/11.7.2/sweetalert2.all.min.js';
 let file:File|null;
 const preview = ref()
 
@@ -26,6 +38,13 @@ function handlePaste(event:ClipboardEvent) {
 
       if (!items || items.length === 0) {
         console.error("当前浏览器不支持本地");
+        //@ts-ignore
+        Sweetalert2.fire({
+          title: 'Error!',
+          text: '当前浏览器不支持本地',
+          icon: 'error',
+          confirmButtonText: '好'
+        })
         return;
       }
       // 搜索剪切板items
@@ -36,6 +55,13 @@ function handlePaste(event:ClipboardEvent) {
         }
       }
       if (!file) {
+        //@ts-ignore
+        Sweetalert2.fire({
+          title: 'Error!',
+          text: '粘贴内容非图片',
+          icon: 'error',
+          confirmButtonText: '好'
+        })
         console.error("粘贴内容非图片");
         return;
       }
@@ -53,6 +79,13 @@ function handlePaste(event:ClipboardEvent) {
 function uploadPlans() {
       if (!file) {
         console.error("请粘贴图片后上传");
+        //@ts-ignore
+        Sweetalert2.fire({
+          title: 'Error!',
+          text: '请粘贴图片后上传',
+          icon: 'error',
+          confirmButtonText: '好'
+        })
         return;
       }
       // loading = true;
@@ -70,6 +103,13 @@ function uploadPlans() {
           console.log('success')
           return res.json();
         } else {
+          //@ts-ignore
+          Sweetalert2.fire({
+          title: 'Error!',
+          text: '服务端异常,请稍后重试',
+          icon: 'error',
+          confirmButtonText: '好'
+        })
           console.log('error')
         }
       }).then(res => {
@@ -84,6 +124,7 @@ function uploadPlans() {
       })
     }
 </script>
-
-<style scoped>
+<style>
+@import url('https://lf9-cdn-tos.bytecdntp.com/cdn/expire-1-M/tailwindcss/2.2.19/tailwind.min.css');
+@import "https://lf9-cdn-tos.bytecdntp.com/cdn/expire-1-M/daisyui/2.2.2/full.min.css";
 </style>
