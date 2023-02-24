d718ed53843f0e603747e9bebc6604269ea191ca
码魂 <pkptzx@gmail.com>
2023-02-25 04:23:03 +0800

    feat: 路由中增加title,改变页面title

--- src/router/index.ts
+++ src/router/index.ts
@@ -6,14 +6,25 @@ const router = createRouter({
     {
       path: "/",
       name: "SpeechRecognition",
+      meta: {
+        title: '语音识别'
+      },
       component: () => import("../views/SpeechRecognition.vue"),
     },
     {
       path: "/qr",
       name: "qr",
+      meta: {
+        title: '二维码解析'
+      },
       component: () => import("../views/QRDecoder.vue"),
     },
   ],
 });
-
+router.beforeEach((to, from, next) => {
+  if (to.meta.title) {
+      document.title = to.meta.title as string;
+  }
+  next();
+});
 export default router;
